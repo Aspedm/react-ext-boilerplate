32f1369e40d3d2df2eed661377ca78048f0d8f41
Тагир Хаджиев <aspedm@gmail.com>
2021-02-14 11:25:11 +0300

    Release 0.0.2

--- src/store/interfaces.ts
+++ src/store/interfaces.ts
@@ -1,38 +1,13 @@
 import { AxiosRequestConfig } from 'axios';
 
-export interface ICaptcha {
-	site_key: string;
-}
-
-export interface ICaptchaErrorObject {
-	captcha: ICaptcha;
-	captcha_phrase_key: string;
-	type: string;
-}
-
-export interface IErrors {
-	error: string;
-	error_description: string;
-	captcha?: ICaptchaErrorObject;
-}
-
 export interface IActionCommonInterface {
 	type: string;
 	payload: {
 		data?: object | object[];
-		errors?: IErrors[];
-		request?: object[];
-		result?: boolean;
 	};
 	config?: AxiosRequestConfig;
 	meta: {
 		ignoreAuth?: boolean;
 		async: boolean;
 	};
-}
-
-export interface IJsonApiResp {
-	data: any;
-	result: boolean;
-	request: object[];
 }
